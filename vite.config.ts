af979cdd8e176f680ba0d7e897f954bfacdb0063
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2026-06-09 10:52:16 -0700

    added RSS feed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { rssPlugin } from "vite-plugin-rss";
 import { defineConfig } from "vite";
+import { loadAllPosts } from "./lib/posts";
+import { JSDOM } from "jsdom"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    rssPlugin({
+      mode: "define",
+      channel: {
+        title: "Vishal Kotcherlakota",
+        link: "https://kotcherlakota.org/"
+      },
+      items: (await loadAllPosts()).map(p => {
+        const dom = new JSDOM()
+        const parser = new dom.window.DOMParser()
+        const doc = parser.parseFromString(p.cleanHTML, "text/html")
+        const description = doc.querySelector("p")?.outerHTML
+        return {
+          title: p.attrs["title"] ?? "(untitled)",
+          link: `https://kotcherlakota.org/blog/${p.slug}`,
+          description
+        }
+      })
+    })
+  ],
   resolve: {
     tsconfigPaths: true,
   },
